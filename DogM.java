efe4721dc9573a1eeee344df2f939a91206a093c
alexkmsk <53709180+alexkmsk@users.noreply.github.com>
2020-07-19 21:20:32 +0300

    Create DogM.java

---
+++ DogM.java
@@ -0,0 +1,29 @@
+public class DogM {
+    String name;
+    int age;
+
+    public void setName (String newName) {
+        this.name = newName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "DogM{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+}
